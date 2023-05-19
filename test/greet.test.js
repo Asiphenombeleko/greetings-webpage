d634cd1641d4392de8d3877374c7b26cb236333b
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-05-19 12:30:51 +0200

    greetings tests

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,4 +1,65 @@
-describe("My Greet function", function(){
-it("should return the name of the person greeted in English",makeGreet())
-assert.equal("Hello",)
+describe("My Greet function", function () {
+  it("should return the name of the person greeted in English", function () {
+    var greeter = greetings();
+    assert.equal("Hello Asisipho", greeter.makeGreet("asisipho", "english"));
+  });
+
+  it("should return the name of the person greeted in isiXhosa", function () {
+    var greeter = greetings();
+    assert.equal("Molo Asisipho", greeter.makeGreet("asisipho", "xhosa"));
+  });
+  it("should return the name of the person greeted in French", function () {
+    var greeter = greetings();
+    assert.equal("Salut Asisipho", greeter.makeGreet("asisipho", "french"));
+  });
+  it("should return the array with names of people greeted", function () {
+    var greeter = greetings();
+    greeter.makeGreet("yamisa", "english");
+    greeter.makeGreet("asisipho", "english");
+    assert.deepEqual(["Yamisa", "Asisipho"], greeter.listOfNamesGreeted());
+
+    greeter.makeGreet("Saffah", "xhosa");
+    greeter.makeGreet("Thembakazi", "xhosa");
+    assert.deepEqual(
+      ["Yamisa", "Asisipho", "Saffah", "Thembakazi"],
+      greeter.listOfNamesGreeted()
+    );
+  });
+  it("should return the length of the greeted names", function () {
+    var greeter = greetings();
+    greeter.makeGreet("yamisa", "english");
+    greeter.makeGreet("Asisipho", "english");
+    greeter.makeGreet("Likhona", "english");
+    greeter.makeGreet("Alunamda", "english");
+    assert.deepEqual(4, greeter.countingNames());
+
+    greeter.makeGreet("Lwandle", "english");
+    assert.deepEqual(5, greeter.countingNames());
+  });
+ 
+});
+describe("My ErrorHandling function", function(){
+    it("should return 'Please enter name & select language'when it is not selected",function(){
+        var greeter = greetings();
+       
+        assert.equal("Please enter name & select language", greeter.errorHandling('',''))
+    })    
+
+    it("should return 'Please select the language!'when no language is entered",function(){
+        var greeter = greetings();
+       
+        assert.equal("Please select the language!", greeter.errorHandling('Lwandle',''))
+    })    
+
+    it("should return 'Please enter name!'when no name is entered",function(){
+        var greeter = greetings();
+       
+        assert.equal("Please enter your name!", greeter.errorHandling('','xhosa'))
+    })    
+
+    it("should return 'please enter correct details!'when user enters characters that are not alphabets",function(){
+        var greeter = greetings();
+       
+        assert.equal("please enter correct details!", greeter.errorHandling('Asiphe22','french'))
+    })    
 })
